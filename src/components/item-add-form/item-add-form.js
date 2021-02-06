f403d6e18310d9cd992325cb22aa527694efd1b2
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-06 18:03:08 +0300

    create form to add custom elements

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,19 +4,34 @@ import './item-add-form.css';
 
 class ItemAddForm extends Component {
 
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({ label: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addItem(this.state.label);
+    e.target.reset();
+  };
+
 	render() {
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
         <input 
           type='text'
-          className="form-control add-input"
-          placeholder="Add new item"></input>
+          className="form-control"
+          placeholder="Add new item"
+          onChange={ this.onLabelChange }></input>
         <button 
-          className="btn btn-outline-secondary"
-          onClick={ () => this.props.addItem('hello') }>
+          className="btn btn-outline-secondary">
           Add Item
         </button>
-      </div>
+      </form>
     );
   };
 };
