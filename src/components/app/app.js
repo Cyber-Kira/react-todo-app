bf3feb734ec47ead17a14badd37cac34cae634a4
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-06 17:50:16 +0300

    change state in app with toggle functions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,11 +13,20 @@ class App extends Component {
 
 	state = {
 		todoData: [
-			{ label: 'Drink Coffee', important: false, id: 1 },
-			{ label: 'Make Awesome App', important: true, id: 2 },
-			{ label: 'Have a lunch', important: false, id: 3 }
+			this.createTodoItem('Drink Coffee'),
+			this.createTodoItem('Make Awesome App'),
+			this.createTodoItem('Have a lunch')
 		]
 	};
+
+	createTodoItem(label) {
+		return {
+			label: label,
+			important: false, 
+			id: this.maxId += 1,
+			done: false
+		}
+	}
 
 	deleteItem = (id) => {
 		this.setState(({ todoData }) => {
@@ -33,11 +42,7 @@ class App extends Component {
 	};
 
 	addItem = (text) => {
-		const newItem = {
-			label: text,
-			important: false, 
-			id: this.maxId += 1
-		}
+		const newItem = this.createTodoItem(text);
 
 		this.setState(({ todoData }) => {
 			return {
@@ -49,19 +54,44 @@ class App extends Component {
 		});
 	};
 
-	onToggleImportant = (id) => {
-		console.log('toggle imp', id);
-	};
+	toggleProperty = (arr, id, propName) => {
+		const idx = arr.findIndex((el) => el.id === id);
+
+		const oldItem = arr[idx];
+		const newItem = {...oldItem, [propName]: !oldItem[propName]};
+
+		return [
+				...arr.slice(0, idx),
+				newItem, 
+				...arr.slice(idx + 1)
+			]
+	}
 
 	onToggleDone = (id) => {
-		console.log('toggle done', id);
+		this.setState(({todoData}) => {
+			return {
+				todoData: this.toggleProperty(todoData, id, 'done')
+			}
+		});
 	};
+
+	onToggleImportant = (id) => {
+		this.setState(({todoData}) => {
+			return {
+				todoData: this.toggleProperty(todoData, id, 'important')
+			}
+		});
+	};
+
 
 	render() {
 		const { todoData } = this.state;
+		const doneCount = todoData.filter((el) => el.done).length;
+		const todoCount = todoData.length - doneCount;
+
 		return (
 			<div className="todo-app">
-				<AppHeader toDo={1} done={3} />
+				<AppHeader toDo={todoCount} done={doneCount} />
 
 				<div className="top-panel d-flex">
 				<SearchPanel />
